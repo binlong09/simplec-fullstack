67de313031920ab0cbe0cf4a5739673e582ef266
binlong09 <binlong09@gmail.com>
2019-03-13 21:04:54 -0400

    added router for navigation

---
+++ client/src/pages/Authentication.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class Authentication extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Authentication</h3>
+      </div>
+    )
+  }
+}
+
+export default Authentication
